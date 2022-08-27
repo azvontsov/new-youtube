3ffb64ca40e4bddcb47976e917b03573df607f87
anton <azvontsov@gmail.com>
2022-08-27 11:59:48 -0400

    adds navbar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}></Stack>
+);
 
 export default Feed;
