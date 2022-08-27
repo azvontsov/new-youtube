bb99e0e4bbe300f978d034c3baae888100d8736b
anton <azvontsov@gmail.com>
2022-08-27 13:18:06 -0400

    adds fetchApi, selected categories

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,26 +1,50 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-    <Box
-      sx={{
-        height: { sx: "auto", md: "92vh" },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <Sidebar />
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#fff" }}
+import { fetchFromApi } from "../utils/fetchFromApi";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
       >
-        Copyright 2022 Anton Media
-      </Typography>
-    </Box>
-  </Stack>
-);
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 Anton Media
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#7b61ff" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Videos = () => <div>fwd</div>;
+
+export default Videos;
